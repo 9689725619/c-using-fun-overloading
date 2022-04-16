c98942d9f69922e87f71714fb0d4d335691534eb
Shubhangi meshram <101330420+9689725619@users.noreply.github.com>
2022-04-16 14:42:31 +0530

    Add files via upload

---
+++ c++ fun overloading.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+void test(int);
+void test(float);
+int main()
+{
+	int a=6;
+	float b=3.1452;
+	test( a);    test(b);    test(a,b);
+	return 0;}
+	void test(int a){
+		cout<<"\n integer value"<<<a;}
+		void test(float b){
+			cout<<"\n float value"<<b;	
+		}
+		void test(inta,intb);
+		cout<<"\n integer value"<<a;
+		cout<<"\n float value"<<b;
+		
+		
+	}
+	
+
